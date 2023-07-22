f0afe4c76bb89ea045b1ff34fc605cd39e9b586b
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-22 19:24:36 -0400

    the spring app serves web content

---
+++ src/main/java/com/example/spring/ContentController.java
@@ -0,0 +1,15 @@
+package com.example.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ContentController {
+    @GetMapping("/content")
+    public String content(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "content";
+    }
+}
